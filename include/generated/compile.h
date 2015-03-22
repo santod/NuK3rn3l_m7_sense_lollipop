d83d3706c7f6b6ae3ef8a572d27f3a5231942ab7
santod <santod040@gmail.com>
2015-03-21 17:00:58 -0700

    Initial HTC m7 Dev source commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 3 10:51:48 PST 2015"
+#define LINUX_COMPILE_BY "santod"
+#define LINUX_COMPILE_HOST "DevBoX"
+#define LINUX_COMPILER "gcc version 4.7.4 20140323 (prerelease) (SM-Toolchain) "
